e6bfd5e351f0fecb687a1b07f5063615f0118e6a
Ulysse <ulyssewaintal@gmail.com>
2022-11-02 00:12:25 +0100

    Created debug functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include <string>
 
+#include "board.h"
+
+using namespace std;
+
 int main() {
-	cout << "Hello World!" << endl;
+	cout << "Je suis une licorne" << endl;
+	Board board;
+	cout << board.toString() << endl;
+	cout << board.getStringEmptySquares() << endl;
+
+	board.setSquare({2, 0}, 128);
+	cout << board.toString() << endl;
+	cout << board.getStringEmptySquares() << endl;
+
+	return 1;
 }
 
